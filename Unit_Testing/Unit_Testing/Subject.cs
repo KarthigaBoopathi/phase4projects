b09be0413d5a5b9891edd105edbf9bdaa894bfa4
Karthiga <karthigaboopathi2000@gmail.com>
2022-11-01 12:36:22 +0530

    unittesting_project

---
+++ Unit_Testing/Unit_Testing/Subject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Testing
+{
+    public class Subject
+    {
+        public int SubjectId { get; set; }
+        public string SubjectName { get; set; }
+        public int SubjectMarks { get; set; }
+    }
+}
---
+++ Unit_Testing/Unit_TestingTests/TestClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Unit_Testing;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace Unit_TestingTests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [TestCase]
+        public void Test_student()
+        {
+            Student stu = new Student();
+            stu.StudentId = 90;
+            stu.StudentName = "Karthiga";
+            Assert.AreEqual(stu.StudentId, 90);      
+        }
+
+        [TestCase]
+        public void Test_subject()
+        {
+            Subject sub = new Subject();
+            sub.SubjectId = 101;
+            sub.SubjectName = "Mathematics";
+            sub.SubjectMarks = 100;
+            Assert.AreEqual(sub.SubjectMarks, 100);
+        }
+
+        [TestCase]
+        public void Test_teacher()
+        {
+            Teacher tea = new Teacher();
+            tea.TeacherId = 11;
+            tea.TeacherName = "Silambarasan";
+            Assert.AreEqual(tea.TeacherName, "Silambarasan");
+        }
+
+    }
+}
